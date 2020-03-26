11004c2a2652aa35b7ef61dbc00457264e41dc18
GeoCloudUtils <37254300+GeoCloudUtils@users.noreply.github.com>
2020-03-27 00:13:02 +0200

    First Commit

---
+++ GameScripts 1/BallController.cs
@@ -0,0 +1,24 @@
+﻿using ScriptUtils.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D ballRb;
+    public string hitTagName;
+    void Start()
+    {
+        gameObject.AddComponent<ColliderEventSystem>().ColliderEntered += BallController_ColliderEntered;
+    }
+
+    private void BallController_ColliderEntered(ColliderEventSystem eventTarget, Collider2D other)
+    {
+        if (other.tag == hitTagName)
+        {
+            ballRb.bodyType = RigidbodyType2D.Kinematic;
+            ballRb.velocity = Vector2.zero;
+            ballRb.angularVelocity = 0f;
+        }
+    }
+}
---
+++ GameScripts/InterfaceManager.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using ScriptUtils.GameUtils;
+using ScriptUtils.Interface;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public GameObject lScreen;
+    public Button soundButton;
+    public Button musicButton;
+    public Button playButton;
+    public Button reloadButton;
+    public GameObject[] InterfaceObjects;
+    void Start()
+    {
+        soundButton.onClick.AddListener(EnableSound);
+        musicButton.onClick.AddListener(EnableMusic);
+        playButton.onClick.AddListener(Play);
+        reloadButton.onClick.AddListener(DoReload);
+    }
+
+    private void DoReload()
+    {
+        reloadButton.interactable = false;
+        reloadButton.gameObject.SetActive(false);
+        Navigator.getInstance().setLoadingScreenPrefab<LoadingScreen>(lScreen);
+        Navigator.getInstance().LoadLevel("MainGame");
+    }
+    private void Play()
+    {
+        foreach (GameObject go in InterfaceObjects)
+            go.SetActive(false);
+        reloadButton.gameObject.SetActive(true);
+    }
+
+    private void EnableMusic()
+    {
+        musicButton.GetComponent<Image>().color = musicButton.GetComponent<Image>().color != Color.white ? Color.white : Color.red;
+    }
+
+    private void EnableSound()
+    {
+        soundButton.GetComponent<Image>().color = soundButton.GetComponent<Image>().color != Color.white ? Color.white : Color.red;
+    }
+}
