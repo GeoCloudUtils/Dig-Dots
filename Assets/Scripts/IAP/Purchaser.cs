a023a86d2fade15c687e0d83922620bb62aa18a5
GeoCloudUtils <37254300+GeoCloudUtils@users.noreply.github.com>
2020-05-15 15:12:10 +0300

    IAP Implementation

---
+++ Assets/Scripts/IAP/Purchaser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using System;
+
+public class Purchaser : Singleton<Purchaser>
+{
+
+}
